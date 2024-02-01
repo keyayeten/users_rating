b0c57f63b197238544bab916748ae8f7dcf112fc
keyayeten <76627566+keyayeten@users.noreply.github.com>
2024-02-02 01:47:39 +0300

    added admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Post, PostRating
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["author", "text", "date_added"]
+    list_filter = ["date_added", "author"]
+
+
+@admin.register(PostRating)
+class PostRatingAdmin(admin.ModelAdmin):
+    list_display = ["user", "post", "rating"]
+    list_filter = ["user", "post"]
